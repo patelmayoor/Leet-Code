491052e03445ff8d16ff67c51d7f17164ece2c85
patelmayoor <patelmayoor151@gmail.com>
2024-08-05 09:28:35 +0530

    solution.java

--- solution.java
+++ solution.java
@@ -1,3 +1,6 @@
+https://leetcode.com/problems/merge-sorted-array/
+
+
 class solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
         for (int j = 0, i = m; j < n; j++) {
